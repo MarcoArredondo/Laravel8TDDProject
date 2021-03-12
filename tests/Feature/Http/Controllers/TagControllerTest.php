3a7589301b4defdfdaf2042f6f168bf233cd58cb
Marco Arredondo <marcoarredondoz28@gmail.com>
2021-03-12 11:09:28 -0600

    Creación de test para verificar eliminación de BD

--- tests/Feature/Http/Controllers/TagControllerTest.php
+++ tests/Feature/Http/Controllers/TagControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Http\Controllers;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Tag;
 
 class TagControllerTest extends TestCase
 {
@@ -20,5 +21,14 @@ class TagControllerTest extends TestCase
         $this->assertDatabaseHas("tags", [
             "name" => "PHP"
         ]);
+    }
+
+    public function testDestroy()
+    { 
+        $this->withoutExceptionHandling();
+        $tag = Tag::factory()->create();
+        $this->delete("tags/{$tag->id}")->assertRedirect("/");
+
+        $this->assertDatabaseMissing("tags", ["name" => $tag->name]);
     }
 }
